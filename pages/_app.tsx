610f35bdf6e780f0467486c0348509926151036c
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-05 21:07:27 +0200

    complete project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Big_Shoulders_Display, Lexend_Deca} from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const big_shoulder_display = Big_Shoulders_Display({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-big-shoulder',
+})
+
+const lexend_deca = Lexend_Deca({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend-deca',
+})
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <main className={`${big_shoulder_display.variable} ${lexend_deca.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'big-shoulder': ['var(--font-big-shoulder)'],
+        'lex-deca': ['var(--font-lexend-deca)'],
+      },
     },
   },
   plugins: [],
