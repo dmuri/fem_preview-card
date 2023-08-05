efa9e2f7eda5a2b5f230d4e3f7d1069761853eea
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-06 01:25:36 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,21 +10,21 @@ export default function Home() {
             <Image height={90} width={90} src="/images/icon-sedans.svg" alt="icon of car" />
             <h2 className=" font-big-shoulder text-6xl font-semibold text-white">SEDANS</h2>
             <p className="lg:h-48 font-lex-deca text-white leading-8 opacity-80">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-            <button className="bg-white text-[#e28525] hover:bg-[#e28525] hover:text-white border-solid border-2 p-4 font-lex-deca text-white rounded-full mt-16 w-36" >Learn More</button>
+            <button className="bg-white text-[#e28525] hover:bg-[#e28525] hover:text-white border-solid border-2 p-4 font-lex-deca rounded-full mt-16 w-36" >Learn More</button>
           </div>
           <div className="flex flex-col gap-10 max-w-[400px] p-16 bg-[#016972]">
             <Image height={90} width={90} src="/images/icon-suvs.svg" alt="icon of car" />
             <h2 className=" font-big-shoulder text-6xl font-semibold text-white">SUVS</h2>
             <p className="lg:h-48 font-lex-deca text-white leading-8 opacity-80">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation
   and off-road adventures.</p>
-            <button className="bg-white text-[#016972] hover:bg-[#016972] hover:text-white border-solid border-2 p-4 font-lex-deca text-white rounded-full mt-16 w-36" >Learn More</button>
+            <button className="bg-white text-[#016972] hover:bg-[#016972] hover:text-white border-solid border-2 p-4 font-lex-deca rounded-full mt-16 w-36" >Learn More</button>
           </div>
           <div className="flex flex-col gap-10 max-w-[400px] p-16 bg-[#00403f]">
             <Image height={90} width={90} src="/images/icon-sedans.svg" alt="icon of car" />
             <h2 className=" font-big-shoulder text-6xl font-semibold text-white">SEDANS</h2>
             <p className="lg:h-48 font-lex-deca text-white leading-8 opacity-80">Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
   rental and arrive in style.</p>
-            <button className="bg-white text-[#00403f] hover:bg-[#00403f] hover:text-white text-border-solid border-2 p-4 font-lex-deca text-white rounded-full mt-16 w-36" >Learn More</button>
+            <button className="bg-white text-[#00403f] hover:bg-[#00403f] hover:text-white text-border-solid border-2 p-4 font-lex-deca rounded-full mt-16 w-36" >Learn More</button>
           </div>
         </div>
       </div>
