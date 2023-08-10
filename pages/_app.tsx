9fb29cd277959ecc13ffe62102867292024e07cf
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-10 12:10:41 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Big_Shoulders_Display, Lexend_Deca} from "next/font/google";
+import Head from 'next/head'
 
 const big_shoulder_display = Big_Shoulders_Display({
   subsets: ['latin'],
@@ -16,6 +17,9 @@ const lexend_deca = Lexend_Deca({
 
 export default function App({Component, pageProps}: AppProps) {
   return (
+    <Head>
+      <title>Frontend Mentor Challenge</title>
+    </Head>
     <main className={`${big_shoulder_display.variable} ${lexend_deca.variable}`}>
       <Component {...pageProps} />
     </main>
